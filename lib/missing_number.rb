0cc65ac97121c478e85961e595e347b7d68e485b
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-21 09:03:15 -0600

    Removed pry

--- lib/missing_number.rb
+++ lib/missing_number.rb
@@ -1,9 +1,9 @@
 require 'pry'
 class Number
   def self.missing_number(nums)
-    # n = nums.count
-    range = (0..nums.count)
-binding.pry
+    n = nums.count
+    range = (0..n)
+
     a = range.to_a - nums
     a.first
   end
