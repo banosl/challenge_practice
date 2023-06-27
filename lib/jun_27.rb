e6839a6c5e416da2f49242b56da14edd5125a79e
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-27 16:04:14 -0600

    Added #make_change, #decide_coins, and #decide_bills

--- lib/jun_27.rb
+++ lib/jun_27.rb
@@ -14,12 +14,19 @@ class Money
   attr_reader :coins, :cost, :payment
 
   def initialize(cost, payment)
-    @coins = {penny => .01,
-              nickel => .05,
-              dime => .10,
-              quarter => .25}
+    @coins = {penny:  0.01,
+              nickel:  0.05,
+              dime:  0.10,
+              quarter:  0.25}
+    @bills = {one:  1,
+              five:  5,
+              ten:  10,
+              twenty:  20,
+              fifty:  50,
+              hundred:  100}
     @cost = cost
     @payment = payment
+    @currency_change = Hash.new{}
   end
 
   
@@ -32,9 +39,30 @@ class Money
 #then check for nickels, subtract those
 #finally check how many pennies fit in the last bit
 
-  def make_change(cost, payment)
-    remainder = payment - cost
-    binding.pry
+  def make_change
+    remainder = @payment - @cost
+    decimal = (remainder % 1).round(2)
+    whole_num = remainder.to_i
+
+    decide_coins(decimal)
+    decide_bills(whole_num)
+
+    @currency_change.reject!{|k,v| v == 0}
+  end
+
+  def decide_coins(decimal)
+    @coins.reverse_each do |coin, value|
+      times_divisible = ((decimal.round(2))/value).to_i
+      @currency_change[coin] = times_divisible
+      decimal -= (value * times_divisible)
+    end
+  end
+
+  def decide_bills(whole_num)
+    @bills.reverse_each do |bill, value|
+      times_divisible = whole_num/value
+      @currency_change[bill] = times_divisible
+    end
   end
 
 end
@@ -47,4 +75,4 @@ payment1 = 5.00
 
 transaction1 = Money.new(cost1, payment1)
 
-# p transaction1.payment_made
+p transaction1.make_change
