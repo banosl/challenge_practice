8f73832e4d7721f162080b9fa6e9ce352c7d8783
Leo Banos Garcia <bsleo625@gmail.com>
2023-06-28 10:46:02 -0600

    Adds a folder for money problem to work on passing spec, adds #setup_up_change_message and #write_message for money class

---
+++ lib/money_problem/lib/money.rb
@@ -0,0 +1,70 @@
+require 'pry'
+require 'rspec'
+
+class Money
+  attr_reader :coins, :cost, :payment
+
+  def initialize(cost, payment)
+    @coins = {penny:  0.01,
+              nickel:  0.05,
+              dime:  0.10,
+              quarter:  0.25}
+    @bills = {one:  1,
+              five:  5,
+              ten:  10,
+              twenty:  20,
+              fifty:  50,
+              hundred:  100}
+    @cost = cost.delete("$").to_f
+    @payment = payment.delete("$").to_f
+    @currency_change = Hash.new{}
+    @change_message = []
+  end
+
+  def make_change
+    remainder = @payment - @cost
+    decimal = (remainder % 1).round(2)
+    whole_num = remainder.to_i
+
+    decide_bills(whole_num)
+    decide_coins(decimal)
+
+    @currency_change.reject!{|k,v| v == 0}
+
+    set_up_change_message
+    write_message(remainder.round(2))
+  end
+
+  def decide_coins(decimal)
+    @coins.reverse_each do |coin, value|
+      times_divisible = ((decimal.round(2))/value).to_i
+      @currency_change[coin] = times_divisible
+      decimal -= (value * times_divisible)
+    end
+  end
+
+  def decide_bills(whole_num)
+    @bills.reverse_each do |bill, value|
+      times_divisible = whole_num/value
+      @currency_change[bill] = times_divisible
+      whole_num -= (value * times_divisible)
+    end
+  end
+
+  def set_up_change_message
+    @currency_change.each do |denomination, value|
+      if value == 1
+        @change_message << "#{value} #{denomination}"
+      elsif value > 1 && denomination == :penny
+        @change_message << "#{value} pennies"
+      else
+        @change_message << "#{value} in #{denomination}s"
+      end
+    end
+    @change_message
+  end
+
+  def write_message(remainder)
+    p "Your change is $#{remainder} - You are due #{@change_message.join(" and ")}"
+  end
+end
---
+++ lib/money_problem/spec/money_spec.rb
@@ -0,0 +1,90 @@
+require './lib/money.rb'
+
+RSpec.describe Money do
+  it 'can calculate when only cents are due' do
+    total_due   = '$1.82'
+    amount_paid = '$2.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $0.18 - You are due 1 dime and 1 nickel and 3 pennies'
+    
+    expect(calculator.make_change).to eq(expected)
+    
+    total_due   = '$0.73'
+    amount_paid = '$1.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $0.27 - You are due 1 quarter and 2 pennies'
+    
+    expect(calculator.make_change).to eq(expected)
+  end
+
+  xit 'can calculate when only dollars are due' do
+    total_due   = '$2.00'
+    amount_paid = '$10.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $8.00 - You are due 1 five and 3 ones'
+    
+    expect(calculator.make_change).to eq(expected)
+    
+    total_due   = '$5.00'
+    amount_paid = '$20.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $15.00 - You are due 1 ten and 1 five'
+    
+    expect(calculator.make_change).to eq(expected)
+    
+    total_due   = '$4.00'
+    amount_paid = '$5.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $1.00 - You are due 1 one'
+    
+    expect(calculator.make_change).to eq(expected)
+  end
+
+  xit 'can calculate when no change is due' do
+    total_due   = '$2.00'
+    amount_paid = '$2.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'No Change Due'
+    
+    expect(calculator.make_change).to eq(expected)
+  end
+
+  xit 'can calculate when dollars and cents are due' do
+    total_due   = '$2568.75'
+    amount_paid = '$3000.25'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $431.50 - You are due 4 hundreds and 1 twenty and 1 ten and 1 one and 2 quarters'
+    
+    expect(calculator.make_change).to eq(expected)
+    
+    total_due   = '$8.50'
+    amount_paid = '$20.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $11.50 - You are due 1 ten and 1 one and 2 quarters'
+    
+    expect(calculator.make_change).to eq(expected)
+    
+    total_due   = '$28.73'
+    amount_paid = '$30.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $1.27 - You are due 1 one and 1 quarter and 2 pennies'
+    
+    expect(calculator.make_change).to eq(expected)
+  end
+
+  xit 'can calculate penny versus pennies' do
+    total_due   = '$1.99'
+    amount_paid = '$2.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $0.01 - You are due 1 penny'
+    
+    expect(calculator.make_change).to eq(expected)
+
+    total_due   = '$1.98'
+    amount_paid = '$2.00'
+    calculator  = Money.new(total_due, amount_paid)
+    expected    = 'Your change is $0.02 - You are due 2 pennies'
+    
+    expect(calculator.make_change).to eq(expected)
+  end    
+end
